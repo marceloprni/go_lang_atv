7fd0a631fb544f5d8c56eea8ac35578c7ac04e44
Marcelo Perini <marcelo.perini@wcvengenharia.com.br>
2026-07-14 08:54:54 -0300

    vr14072026

--- VR10/cmd/api/main.go
+++ VR10/cmd/api/main.go
@@ -1,62 +1,43 @@
 package main
 
 import (
-	"encoding/json"
+	"emailn/internal/contract"
+	"emailn/internal/domain/campaign"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/render"
 )
-
-type Product struct {
-	ID      int    `json:"id"`
-	Product string `json:"produto"`
-}
-
-type mHandler struct{}
-
-func (m mHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello world handler"))
-}
 
 func main() {
 
 	r := chi.NewRouter()
 
-	m := mHandler{}
-	r.Handle("/hanlder", m)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world"))
-	})
+	service := campaign.Service{}
+	r.Post("/campaigns", func(w http.ResponseWriter, r *http.Request) {
+		var request contract.NewCampaign
+		err := render.DecodeJSON(r.Body, &request)
+		if err != nil {
+			http.Error(w, err.Error(), 400)
+			return
+		}
 
-	r.Get("/{id}", func(w http.ResponseWriter, r *http.Request) {
-		params := chi.URLParam(r, "id")
-		w.Write([]byte("hello world " + params))
-	})
+		id, err := service.Create(request)
 
-	r.Get("/product", func(w http.ResponseWriter, r *http.Request) { // /product?id=2311231
-		params := r.URL.Query().Get("id")
-		w.Write([]byte("hello world " + params))
-	})
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			render.JSON(w, r, map[string]string{"error": err.Error()})
+			return
+		}
 
-	r.Get("/productTotal", func(w http.ResponseWriter, r *http.Request) { // /product/2311231
-		w.Header().Set("Content-Type", "application/json")
-		params := map[string]string{"message": "sucess1"}
-		b, _ := json.Marshal(params)
-		w.Write(b)
-	})
-
-	r.Get("/productTotal2", func(w http.ResponseWriter, r *http.Request) { // /product/2311231
-
-		obj := map[string]string{"message": "sucess2"}
-		render.JSON(w, r, obj)
-	})
-
-	r.Post("/produto", func(w http.ResponseWriter, r *http.Request) {
-		var product Product
-		render.DecodeJSON(r.Body, &product)
-		render.JSON(w, r, product)
+		render.Status(r, 202)
+		render.JSON(w, r, map[string]string{"id": id})
 	})
 
 	http.ListenAndServe(":8080", r)
