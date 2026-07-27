ece6ea79a1a1bb5d891c9bdea01b81e3da4ee1e8
Marcelo Perini <marcelo.perini@wcvengenharia.com.br>
2026-07-27 08:29:01 -0300

    vr27072026

--- VR10/cmd/api/main.go
+++ VR10/cmd/api/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"emailn/internal/contract"
 	"emailn/internal/domain/campaign"
+	"emailn/internal/infrastructure/database"
+	internalerrors "emailn/internal/internal-errors"
+	"errors"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -19,21 +22,28 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	service := campaign.Service{}
+	service := campaign.Service{
+		Repository: &database.CampaignRepository{},
+	}
 	r.Post("/campaigns", func(w http.ResponseWriter, r *http.Request) {
 		var request contract.NewCampaign
-		err := render.DecodeJSON(r.Body, &request)
-		if err != nil {
-			http.Error(w, err.Error(), 400)
-			return
-		}
+		render.DecodeJSON(r.Body, &request)
 
 		id, err := service.Create(request)
 
 		if err != nil {
-			http.Error(w, err.Error(), 500)
+
+			if errors.Is(err, internalerrors.ErrInternal) {
+				render.Status(r, 500)
+
+			} else {
+				render.Status(r, 400)
+
+			}
+
 			render.JSON(w, r, map[string]string{"error": err.Error()})
 			return
+
 		}
 
 		render.Status(r, 202)
